3fc1cb3eedc869c01d7d24f64b26abcb68a05ecc
Shawna Mays <shawnadmays@gmail.com>
2022-09-27 18:27:32 -0400

    this is my even and odd codewars solution

---
+++ evenorodd.js
@@ -0,0 +1,12 @@
+
+//EVEN OR ODD
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(num) {
+    if (num % 2 == 0){
+      return "Even"
+    } else {
+      return "Odd"
+      }
+  }
