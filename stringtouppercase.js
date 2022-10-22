2a69acaed1d8e2830bb44e8deabbfa0040e7e33b
Shawna Mays <shawnadmays@gmail.com>
2022-10-21 20:59:04 -0400

    this is my codewars solution for the string to uppercase problem

---
+++ stringtouppercase.js
@@ -0,0 +1,8 @@
+// CODEWARS
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    const string = (str.toUpperCase());
+     return string
+     
+   }
