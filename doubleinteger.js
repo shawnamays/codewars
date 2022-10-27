19f1933f6b64019741127045b310246415d2716f
Shawna Mays <shawnadmays@gmail.com>
2022-10-26 20:28:29 -0400

    this is my double integer solution

---
+++ doubleinteger.js
@@ -0,0 +1,9 @@
+//double integer solution for you cant code under pressure
+// Code as fast as you can! You need to double the integer and return it.
+
+
+
+function doubleInteger(i) {
+    i = i*2;
+      return i;
+    }
