bb89f5242319e9ca0ead989ef9bcba6f8b2e5cc1
Shawna Mays <shawnadmays@gmail.com>
2022-10-28 19:31:56 -0400

    this is my string ends with? codewars solution

---
+++ stringendswith.js
@@ -0,0 +1,10 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// String Ends With? Solution
+
+function solution(str, ending){
+  
+  
+    return str.endsWith(ending);
+    
+    
+  }
