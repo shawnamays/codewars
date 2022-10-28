b411e0dbd1c9f86a44641aeef35054847280eb41
Shawna Mays <shawnadmays@gmail.com>
2022-10-27 23:33:22 -0400

    this is my lost without a map solution

---
+++ lostwithoutamap.js
@@ -0,0 +1,15 @@
+//Beginner - Lost Without A Map Solution
+
+function maps(x){
+  
+  return x.map (n => n*2);
+  
+  
+//   let newArray = []
+  
+//   for (x=0; newArray.length; x++)
+//     (x*x)
+    
+//     return newArray;
+
+}
