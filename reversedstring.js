b8f92ca94aed3dab729e698fc099e8724c12820b
Shawna Mays <shawnadmays@gmail.com>
2022-10-30 00:17:44 -0400

    this is my reversed string solution resubmit

--- reversedstring.js
+++ reversedstring.js
@@ -1,5 +1,6 @@
 // Reversed Words Codewars Soution
 // Complete the solution so that it reverses all of the words within the string passed in.
+// new saved copy
 
 function reverseWords(str){
     return str.split(' ').reverse().join(' ');
