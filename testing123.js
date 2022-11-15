92ccb77ea4c52b2344b88a42a8fc4a106d91310b
Shawna Mays <shawnadmays@gmail.com>
2022-11-14 22:48:35 -0500

    this is my testing 123 codewars solution

---
+++ testing123.js
@@ -0,0 +1,11 @@
+// testing 123 codewars solution
+
+let number=function(array){
+    return array.map((element, index) => `${index + 1}: ${element}`);
+  }
+
+//Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
