1f14c8d10ec1d966294f57f11481ee0b19a6b282
Shawna Mays <shawnadmays@gmail.com>
2022-11-15 23:03:51 -0500

    this is my messi codewars solution

---
+++ messi.js
@@ -0,0 +1,10 @@
+
+//messi codewards solution
+  
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // return total number of goals in all three leagues
+      return laLigaGoals+copaDelReyGoals+championsLeagueGoals
+    }
+    
+      
+    
