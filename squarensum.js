15f3548d7b27af25a9fd9e37d5495c1ad39e9d13
Shawna Mays <shawnadmays@gmail.com>
2022-12-01 23:58:44 -0500

    this is my square n sum codewars

---
+++ squarensum.js
@@ -0,0 +1,9 @@
+//my squaren sum codewards solution
+
+function squareSum(numbers) {
+    let sum = 0
+    for( let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] **2
+    }
+    return sum
+}
