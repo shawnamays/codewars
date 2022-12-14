f0250d778e81c16a31f92f66f1f2d8d1346106e9
Shawna Mays <shawnadmays@gmail.com>
2022-12-14 01:28:13 -0500

    this is my categorize new members solution

---
+++ newmember.js
@@ -0,0 +1,8 @@
+// CATEGORIZE NEW MEMBER CODEWARS SOLUTION
+
+function openOrSenior(data){
+    function determineMembership(member){
+      return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+    }
+    return data.map(determineMembership);
+  }
